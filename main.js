a0848203ea4a01d6970ad275b73ab457b753bb85
Tzviki Wolvovsky <tzviki@rallybound.com>
2018-06-12 17:50:20 -0400

    added script file

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 window.addEventListener('message', procMessage, false);
 
-            function procMessage(event) {
-                var message = JSON.parse(event.data);
+function procMessage(event) {
+    var message = JSON.parse(event.data);
 
-                if (message.messageType === 'focus') {
-                    var iframe = message.iframe;
-                    var frame = getFrame(iframe);
-                    if (frame) {
-                        frame.document.getElementById(message.elem).focus();
-                    }
-                }
+    if (message.messageType === 'focus') {
+        var iframe = message.iframe;
+        var frame = getFrame(iframe);
+        if (frame) {
+            frame.document.getElementById(message.elem).focus();
+        }
+    }
+}
+
+function getFrame(frameName) {
+    for (var i = 0; i < parent.frames.length; i++) {
+        try {
+            var path = parent.frames[i].location.pathname.replace('/iframe/','');
+            if (path === frameName) {
+                return parent.frames[i];
             }
-            
-            function getFrame(frameName) {
-                for (var i = 0; i < parent.frames.length; i++) {
-                    try {
-                        var path = parent.frames[i].location.pathname.replace('/iframe/','');
-                        if (path === frameName) {
-                            return parent.frames[i];
-                        }
-                    } catch (error) {
-                        continue;
-                    }
-                }
-            }
+        } catch (error) {
+            continue;
+        }
+    }
+}
